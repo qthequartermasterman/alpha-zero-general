25c42452f4f47b382f456ab7a1cf28f9bdbf0d1f
aps0127 <AndrewSansom@my.unt.edu>
2020-06-29 19:34:15 -0500

    Fixed the Imperfect Knowledge NNet to work on both batches and single predictions

--- Uno/pytorch/UnoImperfectKnowledgeNNet.py
+++ Uno/pytorch/UnoImperfectKnowledgeNNet.py
@@ -26,7 +26,7 @@ class UnoImperfectKnowledgeNNet(nn.Module):
         self.hc_relu1 = nn.ReLU()
         self.hc_conv1d2 = nn.Conv1d(64, 64, kernel_size=5, padding=0)
         self.hc_relu2 = nn.ReLU()
-        self.hc_fc1 = nn.Linear(64, 256)
+        self.hc_fc1 = nn.Linear(64*3, 256)
 
         self.combined_fc_pi = nn.Linear(256, self.action_size)
         self.combined_fc_v = nn.Linear(256, 1)
@@ -36,22 +36,27 @@ class UnoImperfectKnowledgeNNet(nn.Module):
         # Extract only the information the NNet should know
         # The only cards it can know are the cards in the discard pile and the current player's hand
         # It also can know the number of cards in each player's hands
-        num_rows, num_columns = board.shape
+        num_rows, num_columns = board.shape[-2], board.shape[-1]
         num_players = num_rows-3
-        known_cards = board[1:4]
-        card_counts_every_container = np.sum(board, axis=1)
+        known_cards = board[..., 1:4, :]
+        card_counts_every_container = torch.sum(board, axis=-1)
         player_ids = [-2 % num_players,  # 2 players back
                       -1 % num_players,  # 1 player forward
                       0,                 # Current Player
                       1 % num_players,   # 1 player forward
                       2 % num_players]   # 2 players forward
-        hand_counts_list = [card_counts_every_container[3 + id] for id in player_ids]
-        hand_counts = np.array(hand_counts_list)
+        hand_counts_list = [card_counts_every_container[..., 3 + id] for id in player_ids]
+        #hand_counts = torch.FloatTensor(hand_counts_list)
+        hand_counts = torch.stack(hand_counts_list).T
         hand_counts = 1 / num_columns * hand_counts
+        hand_counts = hand_counts.unsqueeze(-2)
 
         return known_cards, hand_counts
 
     def forward(self, board):
+        if board.ndim == 2:
+            board = board.unsqueeze(0)
+
         known_cards, hand_counts = self.extract_allowed_information(board)
 
         known_cards = known_cards.view(-1, self.action_size*3)
@@ -60,11 +65,12 @@ class UnoImperfectKnowledgeNNet(nn.Module):
         known_cards = self.b_fc2(known_cards)
         known_cards = self.b_relu2(known_cards)
 
+        # hand_counts = hand_counts.view(1, 1, 5)
         hand_counts = self.hc_conv1d1(hand_counts)
         hand_counts = self.hc_relu1(hand_counts)
         hand_counts = self.hc_conv1d2(hand_counts)
         hand_counts = self.hc_relu2(hand_counts)
-        hand_counts = hand_counts.view(-1, 64)
+        hand_counts = hand_counts.view(-1, 64*3)
         hand_counts = self.hc_fc1(hand_counts)
 
         combined = known_cards*hand_counts
